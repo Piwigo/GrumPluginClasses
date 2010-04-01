32d6442815947b06cd18211b4655189ddb7e2732
ddtddt <ddtddt@piwigo.org>
2010-04-01 19:38:57 +0000

    [extension] GrumPluginClasses - Add ES
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@5551 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['Module'] = 'Módulo';
+$lang['Version'] = 'Versión';
+$lang['List of shared modules'] = 'Lista módulos partidos';
+$lang['List of installed plugins using Grum Plugin Classes'] = 'Lista plugins instalado y que utiliza a Grum Plugin Classes';
+$lang['Plugin'] = 'Plugin';
+$lang['GPC required'] = 'GPC requerido';
+$lang['Installed'] = 'Instalado';
+$lang['Infos'] = 'Informaciones';
+
+?>
