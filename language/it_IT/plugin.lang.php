7334c27927d2c7593191d88ce5b51a0acdfacdc4
riopwg <riopwg@piwigo.org>
2010-04-03 17:56:07 +0000

    Language IT for GrumPluginClasses
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@5604 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['Module'] = 'Modulo';
+$lang['Version'] = 'Versione';
+$lang['List of shared modules'] = 'Elenco dei moduli condivisi';
+$lang['List of installed plugins using Grum Plugin Classes'] = 'Elenco dei plugins installati che usano Grum Plugin Classes';
+$lang['Plugin'] = 'Plugin';
+$lang['GPC required'] = 'GPC richiesto';
+$lang['Installed'] = 'Installato il';
+$lang['Infos'] = 'Informazioni';
+
+?>
