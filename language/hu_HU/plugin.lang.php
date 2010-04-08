5d2f66807fe0c94544c6d688f7b05c86e5b4d2ff
samli <samli@piwigo.org>
2010-04-08 19:05:38 +0000

    [extensions] update HU language
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@5720 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['Module'] = 'Modul';
+$lang['Version'] = 'Verzió';
+$lang['List of shared modules'] = 'Közös modulok listája';
+$lang['List of installed plugins using Grum Plugin Classes'] = 'Telepített bővítmények melyek a Grum Plugin Classes-t használják';
+$lang['Plugin'] = 'Bővítmény';
+$lang['GPC required'] = 'Szükséges GPC';
+$lang['Installed'] = 'Telepítve';
+$lang['Infos'] = 'Információk';
+
+?>
