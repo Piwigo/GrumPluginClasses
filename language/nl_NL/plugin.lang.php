4426bbe9274ac05bc41a6180b27f32f36ea5de10
keeshessels <keeshessels@piwigo.org>
2010-04-13 14:23:41 +0000

    nl_NL added
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@5832 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['Module'] = 'Module';
+$lang['Version'] = 'Versie';
+$lang['List of shared modules'] = 'Lijst van gedeelde modules';
+$lang['List of installed plugins using Grum Plugin Classes'] = 'Lijst met geïnstalleerde plugins die Grum Plugin Classes (GPC) gebruiken';
+$lang['Plugin'] = 'Plugin';
+$lang['GPC required'] = 'GPC nodig';
+$lang['Installed'] = 'Installeer-datum';
+$lang['Infos'] = 'Informatie';
+
+?>
