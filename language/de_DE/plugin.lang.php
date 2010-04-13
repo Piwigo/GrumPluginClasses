060444155ee8b11434ee6f32e993857a9a8e84aa
duke <duke@mycompany.com>
2010-04-13 18:36:06 +0000

    added language de_DE to GPC
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@5839 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['Module'] = 'Modul';
+$lang['Version'] = 'Version';
+$lang['List of shared modules'] = 'Liste der zur Verfügung gestellten Module';
+$lang['List of installed plugins using Grum Plugin Classes'] = 'Liste der installierten Erweiterungen, die Grum Plugin Classes nutzen';
+$lang['Plugin'] = 'Erweiterung';
+$lang['GPC required'] = 'Benötigte GPC Version';
+$lang['Installed'] = 'Installiert am';
+$lang['Infos'] = 'Informationen';
+
+?>
