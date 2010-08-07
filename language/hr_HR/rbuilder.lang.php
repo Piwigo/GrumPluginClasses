7a0be49733fd542323ff69cc765ace6f4cdccb04
ddtddt <ddtddt@piwigo.org>
2010-08-07 13:46:48 +0000

    [extension] GrumPluginClasses, Add hr_HR thanks to LuxZg
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@6747 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hr_HR/rbuilder.lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['gpc_rb_search_criterion']='Kriteriji pretrage';
+$lang['gpc_rb_do_modify_request']='Izmjeni pretragu';
+$lang['gpc_rb_result_query']='Rezultati pretrage';
+$lang['gpc_rb_number_of_item_found']='Broj pronađenih slika';
+$lang['gpc_rb_add_criterions']='Kriteriji';
+$lang['gpc_rb_clear_criterions']='Poništi kriterije';
+$lang['gpc_rb_search']='Pretraži';
+$lang['gpc_rb_textAND']='Svi kriteriji moraju biti potvrđeni';
+$lang['gpc_rb_textOR']='Barem jedan kriterij mora biti odabran';
+
+?>
