cced99ea1decf4b175b54b92a1bd7e2672bc8724
ddtddt <ddtddt@piwigo.org>
2010-09-05 19:31:40 +0000

    [extension] GrumPluginClasses, Add ja_JP (description) thanks to irohaproject
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@6848 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/en_UK/plugin.lang.php
@@ -0,0 +1,11 @@
+<?php
+$lang['Module'] = 'Module';
+$lang['Version'] = 'Version';
+$lang['List of shared modules'] = 'List of shared modules';
+$lang['List of installed plugins using Grum Plugin Classes'] = 'List of installed plugins using Grum Plugin Classes';
+$lang['Plugin'] = 'Plugin';
+$lang['GPC required'] = 'GPC required';
+$lang['Installed'] = 'Installed';
+$lang['Infos'] = 'Infos';
+$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent';
+?>
---
+++ language/ja_JP/rbuilder.lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['gpc_rb_search_criterion']='検索条件';
+$lang['gpc_rb_do_modify_request']='検索変更';
+$lang['gpc_rb_result_query']='検索結果';
+$lang['gpc_rb_number_of_item_found']='画像数';
+$lang['gpc_rb_add_criterions']='条件';
+$lang['gpc_rb_clear_criterions']='条件リセット';
+$lang['gpc_rb_search']='検索';
+$lang['gpc_rb_textAND']='全ての条件が確認すべき';
+$lang['gpc_rb_textOR']='少なくとも１つの条件をチェックする必要がある。';
+
+?>
