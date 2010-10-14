91c385030d1487732d96800512dbcf9f5c831b7d
ddtddt <ddtddt@piwigo.org>
2010-10-14 16:59:59 +0000

    [extension] GrumPluginClasses thanks to dodo / update SK
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@7182 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/rbuilder.lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['gpc_rb_search_criterion']='Vyhľadávacie kritériá';
+$lang['gpc_rb_do_modify_request']='Zmeniť vyhľadávanie';
+$lang['gpc_rb_result_query']='Vyhľadať výsledok';
+$lang['gpc_rb_number_of_item_found']='Počet nájdených fotografií';
+$lang['gpc_rb_add_criterions']='Kritériá';
+$lang['gpc_rb_clear_criterions']='Vymazať kritériá';
+$lang['gpc_rb_search']='Hľadať';
+$lang['gpc_rb_textAND']='Všetky kritériá musia byť overené';
+$lang['gpc_rb_textOR']='Najmenej jedno kritérium musí byť skontrolované';
+
+$lang['gpc_manage_caddie'] = 'Spravovanie Caddie';
+$lang['gpc_add_caddie'] = 'Vyhľadaný výsledok je pridaný do caddie';
+$lang['gpc_replace_caddie'] = 'Vyhľadaný výsledok nahradí caddie';
+$lang['gpc_the_caddie_is_updated'] = 'Caddie bolo aktualizované';
+$lang['gpc_something_is_wrong_on_the_server_side'] = 'Vyskytla sa chyba na strane servera';
+
+
+?>
