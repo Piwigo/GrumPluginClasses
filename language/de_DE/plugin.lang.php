bf9aaccb3f557f2309dcd2cd388c6c14e5332b73
ddtddt <ddtddt@piwigo.org>
2010-10-17 10:04:47 +0000

    [Extension] - GrumPluginClasses - update de_DE thanks to Matterhorn
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@7238 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/de_DE/plugin.lang.php
+++ language/de_DE/plugin.lang.php
@@ -9,4 +9,10 @@ $lang['GPC required'] = 'Benötigte GPC Version';
 $lang['Installed'] = 'Installiert am';
 $lang['Infos'] = 'Informationen';
 $lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'Einige Erweiterungen benötigen Grum Plugin Classes: Sie müssen diese de-installieren, bevor Sie Grum Plugin Classes entfernen';
+
+// gpc > 3.3.0
+$lang['Public'] = 'Oeffentlich';
+$lang['Private'] = 'Privat';
+$lang['All the gallery'] = 'Gesamte Galerie'; 
+
 ?>
