d02d3e40234743acd3a51c1ba62e822d6a4496b0
ddtddt <ddtddt@piwigo.org>
2010-10-24 10:43:33 +0000

    [extensions] - GrumPluginClasses - update DE/IT thanks to sugar888
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@7372 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/de_DE/rbuilder.lang.php
+++ language/de_DE/rbuilder.lang.php
@@ -20,9 +20,9 @@ $lang['gpc_something_is_wrong_on_the_server_side'] = 'Auf dem Server ist ein Feh
 $lang['There is no criteria ! At least, one criteria is required to do search...']="Es gibt kein Kriterium! Es muss mindestens ein Kriterium angegeben werden um die Suche starten zu können...";
 
 // need to be translated ! thanks :-)
-$lang['gpc_help_edit_criteria']="Edit criteria";
-$lang['gpc_help_delete_criteria']="Delete criteria";
-$lang['gpc_help_move_criteria']="Drag'n'drop to organize search conditions";
-$lang['gpc_help_switch_condition']="Invert the nature of the condition (AND &hArr; OR)";
+$lang['gpc_help_edit_criteria']="Kriterium bearbeiten";
+$lang['gpc_help_delete_criteria']="Kriterium löschen";
+$lang['gpc_help_move_criteria']="Drag'n'drop um die Suchbedingungen zu organisieren";
+$lang['gpc_help_switch_condition']="Keheren Sie die Art der Bedingung um (UND &hArr; ODER)";
 
 ?>
--- language/fr_FR/rbuilder.lang.php
+++ language/fr_FR/rbuilder.lang.php
@@ -19,7 +19,7 @@ $lang['gpc_something_is_wrong_on_the_server_side'] = 'Une anomalie est survenue 
 // GPC 3.3.2
 $lang['There is no criteria ! At least, one criteria is required to do search...']="Il n'y a aucun critères ! Au minimum, un critère est requis pour pouvoir effectuer une recherche...";
 
-$lang['gpc_help_edit_criteria']="Editer le critère";
+$lang['gpc_help_edit_criteria']="Éditer le critère";
 $lang['gpc_help_delete_criteria']="Supprimer le critère";
 $lang['gpc_help_move_criteria']="Glisser/Déposer pour organiser les conditions de recherche";
 $lang['gpc_help_switch_condition']="Inverser la nature de la condition (ET &hArr; OU)";
