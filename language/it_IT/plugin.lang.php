840212eb9c9234b42fb6ffe5ce0d3c83cdfdcd73
ddtddt <ddtddt@piwigo.org>
2010-10-24 08:53:09 +0000

    [extensions] - GrumPluginClasses - update IT thanks to sugar888
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@7368 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -7,6 +7,8 @@ $lang['Plugin'] = 'Plugin';
 $lang['GPC required'] = 'GPC richiesto';
 $lang['Installed'] = 'Installato il';
 $lang['Infos'] = 'Informazioni';
+$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'Alcuni plug-in dipendono da Grum Plugin Classes. Prima di disinstallare Grum Plugin Classes, si devono disinstallare i plug-in dipendenti da questo';
+
 
 // gpc > 3.3.0
 $lang['Public'] = 'Pubblico';
