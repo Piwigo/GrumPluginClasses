667db8cebb61ca8bc44d8e4f463fa0ec7247a2eb
ddtddt <ddtddt@piwigo.org>
2010-10-30 15:56:24 +0000

    [extensions] - GrumPluginClasses - update format -> UTF8
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@7503 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pl_PL/plugin.lang.php
+++ language/pl_PL/plugin.lang.php
@@ -1,17 +1,17 @@
 <?php
-$lang['Module'] = 'Modu³';
+$lang['Module'] = 'ModuÂ³';
 $lang['Version'] = 'Wersja';
-$lang['List of shared modules'] = 'Lista wspó³dzielonych modu³ów';
-$lang['List of installed plugins using Grum Plugin Classes'] = 'Lista zainstalowanych wtyczek korzystaj¹cych z Grum Plugin Classes';
+$lang['List of shared modules'] = 'Lista wspÃ³Â³dzielonych moduÂ³Ã³w';
+$lang['List of installed plugins using Grum Plugin Classes'] = 'Lista zainstalowanych wtyczek korzystajÂ¹cych z Grum Plugin Classes';
 $lang['Plugin'] = 'Wtyczka';
 $lang['GPC required'] = 'GPC wymagane';
 $lang['Installed'] = 'Zainstalowane';
 $lang['Infos'] = 'Informacje';
-$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'Niektóre wtyczki s¹ zale¿ne od Grum Plugin Classes: przed odinstalowaniem, odinstaluj wszystkie zale¿ne wtyczki';
+$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'NiektÃ³re wtyczki sÂ¹ zaleÂ¿ne od Grum Plugin Classes: przed odinstalowaniem, odinstaluj wszystkie zaleÂ¿ne wtyczki';
 
 // gpc > 3.3.0
 $lang['Public'] = 'Publiczne';
 $lang['Private'] = 'Prywatne';
-$lang['All the gallery'] = 'Ca³a galeria';
+$lang['All the gallery'] = 'CaÂ³a galeria';
 
 ?>
