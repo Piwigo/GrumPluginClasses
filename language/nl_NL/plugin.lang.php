89bb70da0a4fcd1c01916cc3d81afd6db6ed4c9e
ddtddt <ddtddt@piwigo.org>
2011-05-07 10:47:03 +0000

    [extensions] - GrumPluginClasses - update NL thanks to j.commelin
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@10802 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -1,5 +1,4 @@
 <?php
-
 $lang['Module'] = 'Module';
 $lang['Version'] = 'Versie';
 $lang['List of shared modules'] = 'Lijst van gedeelde modules';
@@ -8,5 +7,10 @@ $lang['Plugin'] = 'Plugin';
 $lang['GPC required'] = 'GPC nodig';
 $lang['Installed'] = 'Installeer-datum';
 $lang['Infos'] = 'Informatie';
+$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'Sommige plugins zijn afhankelijk van Grum Plugin Classes: voordat u deinstalleerd moet u de afhankelijke plugins deinstalleren';
+
+$lang['Public'] = 'Publiek';
+$lang['Private'] = 'Prive';
+$lang['All the gallery'] = 'Heel de galerij';
 
 ?>
