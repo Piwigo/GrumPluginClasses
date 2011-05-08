920e8489bebdbd92c835876dc8e31c67cb28e242
ddtddt <ddtddt@piwigo.org>
2011-05-08 18:41:52 +0000

    [extensions] - GrumPluginClasses - update NL thanks to j.commelin
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@10820 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/nl_NL/rbuilder.lang.php
+++ language/nl_NL/rbuilder.lang.php
@@ -1,26 +1,24 @@
 <?php
-// need to be translated ! thanks :-)
 
-$lang['gpc_rb_search_criterion']='Search criteria';
-$lang['gpc_rb_do_modify_request']='Modify search';
-$lang['gpc_rb_result_query']='Search result';
-$lang['gpc_rb_number_of_item_found']='Number of images found';
-$lang['gpc_rb_add_criterions']='Criteria';
-$lang['gpc_rb_clear_criterions']='Reset criteria';
-$lang['gpc_rb_search']='Search';
-$lang['gpc_rb_textAND']='All criteria must be verified';
-$lang['gpc_rb_textOR']='At least one criterion must be checked';
+$lang['gpc_rb_search_criterion'] = 'Zoekcriteria';
+$lang['gpc_rb_do_modify_request'] = 'Wijzig zoekopdracht';
+$lang['gpc_rb_result_query'] = 'Zoekresultaten';
+$lang['gpc_rb_number_of_item_found'] = 'Aantal gevonden afbeeldingen';
+$lang['gpc_rb_add_criterions'] = 'Criteria';
+$lang['gpc_rb_clear_criterions'] = 'Reset criteria';
+$lang['gpc_rb_search'] = 'Zoek';
+$lang['gpc_rb_textAND'] = 'Alle criteria moeten voldoen';
+$lang['gpc_rb_textOR'] = 'Ten minste een criterium moet voldoen';
+$lang['gpc_manage_caddie'] = 'Caddie-beheer';
+$lang['gpc_add_caddie'] = 'Zoekresultaat is toegevoegd aan de caddie';
+$lang['gpc_replace_caddie'] = 'Zoekresultaat vervangt de caddie';
+$lang['gpc_the_caddie_is_updated'] = 'Caddie is bijgewerkt';
+$lang['gpc_something_is_wrong_on_the_server_side'] = 'Een fout is opgetreden bij de server';
+$lang['There is no criteria ! At least, one criteria is required to do search...'] = 'Er is geen criterium! Ten minste een criterium is nodig om te kunnen zoeken...';
+$lang['gpc_help_edit_criteria'] = 'Wijzig criteria';
+$lang['gpc_help_delete_criteria'] = 'Verwijder criteria';
+$lang['gpc_help_move_criteria'] = 'Drag\'n\'drop om de zoekcriteria te ordenen';
+$lang['gpc_help_switch_condition'] = 'Inverteer de conditie (AND &hArr; OR)';
 
-$lang['gpc_manage_caddie'] = 'Caddie management';
-$lang['gpc_add_caddie'] = 'Search result is added in the caddie';
-$lang['gpc_replace_caddie'] = 'Search result replace the caddie';
-$lang['gpc_the_caddie_is_updated'] = 'Caddie was updated';
-$lang['gpc_something_is_wrong_on_the_server_side'] = 'An error has occured on the server-side';
-$lang['There is no criteria ! At least, one criteria is required to do search...']="There is no criteria ! At least, one criteria is required to do search...";
-
-$lang['gpc_help_edit_criteria']="Edit criteria";
-$lang['gpc_help_delete_criteria']="Delete criteria";
-$lang['gpc_help_move_criteria']="Drag'n'drop to organize search conditions";
-$lang['gpc_help_switch_condition']="Invert the nature of the condition (AND &hArr; OR)";
 
 ?>
