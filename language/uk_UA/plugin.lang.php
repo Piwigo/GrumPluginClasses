143104c6e3019e12ad12a6d91e3f879335d975c9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-03-26 17:20:51 +0000

    [Grum Plugins Classes] Add uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@13755 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['All the gallery'] = 'Уся галерея';
+$lang['GPC required'] = 'Необхідний GPC';
+$lang['Infos'] = 'Інформація';
+$lang['Installed'] = 'Встановлено';
+$lang['List of installed plugins using Grum Plugin Classes'] = 'Список встановлених плаґінів, що використовують Grum Plugin Classes';
+$lang['List of shared modules'] = 'Список спільних модулів';
+$lang['Module'] = 'Модуль';
+$lang['Plugin'] = 'Плаґін';
+$lang['Private'] = 'Приватний';
+$lang['Public'] = 'Публічний';
+$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'Деякі плаґіни, які залежать від Grum Plugin Classes: до видалення, ви повинні спочатку видалити залежні плаґіни';
+$lang['Version'] = 'Версія';
+?>
---
+++ language/uk_UA/rbuilder.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['gpc_add_caddie'] = 'Результат пошуку додається в кошик';
+$lang['gpc_help_delete_criteria'] = 'Видалити критерії';
+$lang['gpc_help_edit_criteria'] = 'Змінити критерії';
+$lang['gpc_help_move_criteria'] = 'Перетяніть, щоб організувати умови пошуку';
+$lang['gpc_help_switch_condition'] = 'Перевернути природні умови (AND &hArr; OR)';
+$lang['gpc_manage_caddie'] = 'Управління кошиком';
+$lang['gpc_rb_add_criterions'] = 'Критерії';
+$lang['gpc_rb_clear_criterions'] = 'Скинути критерії';
+$lang['gpc_rb_do_modify_request'] = 'Змінення пошуку';
+$lang['gpc_rb_number_of_item_found'] = 'Кільість знайдених зображень';
+$lang['gpc_rb_result_query'] = 'Результат пошуку';
+$lang['gpc_rb_search'] = 'Пошук';
+$lang['gpc_rb_search_criterion'] = 'Критерії пошуку';
+$lang['gpc_rb_textAND'] = 'Всі критерії повинні бути перевірені';
+$lang['gpc_rb_textOR'] = 'Принаймні один критерій повинен бути перевіреним';
+$lang['gpc_replace_caddie'] = 'Результат пошуку замінити на кошик';
+$lang['gpc_something_is_wrong_on_the_server_side'] = 'Сталася помилка на стороні сервера';
+$lang['gpc_the_caddie_is_updated'] = 'Кошик був оновлений';
+$lang['There is no criteria ! At least, one criteria is required to do search...'] = 'Немає жодного критерія! Потрібен принаймі один критерій для пошуку...';
+?>
