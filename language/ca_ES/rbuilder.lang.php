f63dc8fcf48404a445db1c4490e6b84cbb1e4c09
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-04 23:24:55 +0000

    [grum_plugins_classes] Update ca_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@14758 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/rbuilder.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['There is no criteria ! At least, one criteria is required to do search...'] = 'No s\'ha introduït cap criteri per a dur a terme la cerca. És imprescindible introduïr-ne un.';
+$lang['gpc_add_caddie'] = 'El resultat de la cerca s\'ha afegit al carret';
+$lang['gpc_help_delete_criteria'] = 'Eliminar criteris';
+$lang['gpc_help_edit_criteria'] = 'Editar criteris';
+$lang['gpc_help_move_criteria'] = 'Arrossegar i deixar anar per organitzar els termes de cerca';
+$lang['gpc_help_switch_condition'] = 'Invertir la naturalesa de la condició (I &hArr; O)';
+$lang['gpc_manage_caddie'] = 'Gestionar carret';
+$lang['gpc_rb_add_criterions'] = 'Criteris';
+$lang['gpc_rb_clear_criterions'] = 'Restablir criteris';
+$lang['gpc_rb_do_modify_request'] = 'Modificar cerca';
+$lang['gpc_rb_number_of_item_found'] = 'Nombre d\'imatges trobades';
+$lang['gpc_rb_result_query'] = 'Resultat de la cerca';
+$lang['gpc_rb_search_criterion'] = 'Criteris de cerca';
+$lang['gpc_rb_search'] = 'Cercar';
+$lang['gpc_rb_textAND'] = 'Tots els criteris han de ser verificats';
+$lang['gpc_rb_textOR'] = 'Com a mínim un criteri s\'ha de revisar';
+$lang['gpc_replace_caddie'] = 'El resultat de la cerca substitueix el carret';
+$lang['gpc_something_is_wrong_on_the_server_side'] = 'S\'ha produït un error en el costat del servidor';
+$lang['gpc_the_caddie_is_updated'] = 'El carret s\'ha actualitzat';
+?>
