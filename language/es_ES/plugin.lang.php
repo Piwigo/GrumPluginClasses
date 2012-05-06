3971ce96db4e7553c885851ebdbc244ab953ba80
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-06 10:12:30 +0000

    [grum_plugins_classes] Update es_ES, thanks to : crazydark
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@14796 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/plugin.lang.php
+++ language/es_ES/plugin.lang.php
@@ -2,16 +2,17 @@
 
 $lang['Module'] = 'Módulo';
 $lang['Version'] = 'Versión';
-$lang['List of shared modules'] = 'Lista módulos partidos';
-$lang['List of installed plugins using Grum Plugin Classes'] = 'Lista plugins instalado y que utiliza a Grum Plugin Classes';
+$lang['List of shared modules'] = 'Lista de módulos partidos';
+$lang['List of installed plugins using Grum Plugin Classes'] = 'Lista de plugins instalados y que utilizan a Grum Plugin Classes';
 $lang['Plugin'] = 'Plugin';
 $lang['GPC required'] = 'GPC requerido';
 $lang['Installed'] = 'Instalado';
-$lang['Infos'] = 'Informaciones';
+$lang['Infos'] = 'Informacion';
 
 // gpc > 3.3.0
 $lang['Public'] = 'Publico';
 $lang['Private'] = 'Privado';
 $lang['All the gallery'] = 'Toda la galeria';
 
+$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'Algunos plugins son dependientes de otros plugins de clases superior: antes de desinstalarlos, tu tienes que desinstalar los plugins de los que dependen';
 ?>
