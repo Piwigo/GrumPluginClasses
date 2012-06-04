0c1eac0d7013c547fc5b7320d11769a7be440a48
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-04 13:02:57 +0000

    [grum_plugins_classes] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@15526 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['All the gallery'] = 'Όλες οι γκαλερί';
+$lang['GPC required'] = 'GPC απαιτούμενο';
+$lang['Infos'] = 'Πληροφορίες';
+$lang['Installed'] = 'Εγκατεστημένο';
+$lang['List of installed plugins using Grum Plugin Classes'] = 'Λίστα με εγκατεστημένα πρόσθετα που χρησιμοποιούν το Grum Plugin Classes';
+$lang['List of shared modules'] = 'Κατάλογος των κοινών ενοτήτων';
+$lang['Module'] = 'Ενότητα';
+$lang['Plugin'] = 'Πρόσθετο';
+$lang['Private'] = 'Ιδιωτικό';
+$lang['Public'] = 'Δημόσιο';
+$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'Μερικές πρόσθετα εξαρτώνται από το Grum Plugin Classes: πριν την απεγκατάσταση, θα πρέπει πρώτα να απεγκαταστήσετε τα πρόσθετα που εξαρτώνται.';
+$lang['Version'] = 'Έκδοση';
+?>
---
+++ language/el_GR/rbuilder.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['gpc_add_caddie'] = 'Τα αποτέλεσμα αναζήτησης προστίθεται στο Caddie';
+$lang['gpc_manage_caddie'] = 'Διαχείριση Caddie';
+$lang['gpc_rb_add_criterions'] = 'Κριτήρια';
+$lang['gpc_rb_clear_criterions'] = 'Επαναφορά κριτηρίων';
+$lang['gpc_rb_do_modify_request'] = 'Τροποποίηση αναζήτησης';
+$lang['gpc_rb_number_of_item_found'] = 'Αριθμός των εικόνων που βρέθηκαν';
+$lang['gpc_rb_result_query'] = 'Αποτέλεσμα αναζήτησης';
+$lang['gpc_rb_search_criterion'] = 'Τα κριτήρια αναζήτησης';
+$lang['gpc_rb_search'] = 'Αναζήτηση';
+$lang['gpc_rb_textAND'] = 'Όλα τα κριτήρια πρέπει να επαληθεύονται';
+$lang['gpc_rb_textOR'] = 'Τουλάχιστον ένα από τα κριτήρια πρέπει να ελεγχθούν';
+?>
