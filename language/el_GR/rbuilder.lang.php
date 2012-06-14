6d60856df7f5960e800d6b3cf3232a793cc68390
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-14 22:30:51 +0000

    [grum_plugins_classes] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@15702 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/el_GR/rbuilder.lang.php
+++ language/el_GR/rbuilder.lang.php
@@ -31,4 +31,12 @@ $lang['gpc_rb_search_criterion'] = 'Τα κριτήρια αναζήτησης';
 $lang['gpc_rb_search'] = 'Αναζήτηση';
 $lang['gpc_rb_textAND'] = 'Όλα τα κριτήρια πρέπει να επαληθεύονται';
 $lang['gpc_rb_textOR'] = 'Τουλάχιστον ένα από τα κριτήρια πρέπει να ελεγχθούν';
+$lang['There is no criteria ! At least, one criteria is required to do search...'] = 'Δεν υπάρχουν κριτήρια! Τουλάχιστον, ένα κριτήριο απαιτείται για να γίνει αναζήτηση ...';
+$lang['gpc_help_delete_criteria'] = 'Διαγραφή κριτηρίων';
+$lang['gpc_help_edit_criteria'] = 'Επεξεργασία κριτηρίων';
+$lang['gpc_help_move_criteria'] = 'Μεταφορά και απόθεση για οργανωση απαιτήσεων αναζήτησης';
+$lang['gpc_help_switch_condition'] = 'Αναστροφή της ουσίας των όρων (AND &hArr; OR)';
+$lang['gpc_replace_caddie'] = 'Τα αποτελέσματα αναζήτησης αντκαθηστούν το καλάθι';
+$lang['gpc_something_is_wrong_on_the_server_side'] = 'Παρουσιάστηκε λάθος στην πλευρά του διακομιστή';
+$lang['gpc_the_caddie_is_updated'] = 'Το καλάθι ενημερώθηκε';
 ?>
