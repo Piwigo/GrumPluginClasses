04ddb81c63c4dab0773756ab18dc48f3c066caff
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-14 22:30:23 +0000

    [grum_plugins_classes] Update ja_JP, thanks to : oyoyon
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@15699 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ja_JP/plugin.lang.php
+++ language/ja_JP/plugin.lang.php
@@ -9,4 +9,6 @@ $lang['GPC required'] = 'GPC必要';
 $lang['Installed'] = 'インストールされていた';
 $lang['Infos'] = '情報';
 $lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'いくつかのプラグインはGrum Plugin Classesに付属している：アンインストールする前に、まず、プラグインの付属をアンインストールする必要がある。';
+$lang['Private'] = 'プライベート';
+$lang['Public'] = 'パブリック';
 ?>
