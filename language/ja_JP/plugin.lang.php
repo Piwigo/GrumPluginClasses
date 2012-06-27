fbcbf368902a4cfae09ef42ac20daa0f3191e226
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-27 19:16:41 +0000

    [grum_plugins_classes] Update ja_JP, thanks to : oyoyon
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@16096 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ja_JP/plugin.lang.php
+++ language/ja_JP/plugin.lang.php
@@ -5,10 +5,11 @@ $lang['Version'] = 'バージョン';
 $lang['List of shared modules'] = '共有モジュールのリスト';
 $lang['List of installed plugins using Grum Plugin Classes'] = 'Grum Plugin Classesを利用し、インストールされているプラグインのリスト';
 $lang['Plugin'] = 'プラグイン';
-$lang['GPC required'] = 'GPC必要';
+$lang['GPC required'] = 'GPCが必要';
 $lang['Installed'] = 'インストールされていた';
 $lang['Infos'] = '情報';
 $lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'いくつかのプラグインはGrum Plugin Classesに付属している：アンインストールする前に、まず、プラグインの付属をアンインストールする必要がある。';
 $lang['Private'] = 'プライベート';
 $lang['Public'] = 'パブリック';
+$lang['All the gallery'] = '全てのギャラリー';
 ?>
