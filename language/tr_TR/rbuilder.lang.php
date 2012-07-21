0f6edc652ca042e580742a091ac7748627123fd5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-21 07:29:10 +0000

    [grum_plugins_classes] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@16882 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/rbuilder.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['There is no criteria ! At least, one criteria is required to do search...'] = 'Kriter yok! Arama için en az bir adet kriter gerekmektedir.';
+$lang['gpc_add_caddie'] = 'Arama sonuçları sepete eklendi';
+$lang['gpc_help_delete_criteria'] = 'Kriteri sil';
+$lang['gpc_help_edit_criteria'] = 'Kriteri düzenle';
+$lang['gpc_help_move_criteria'] = 'Arama durumlarını organize etmek için sürükle ve bırak';
+$lang['gpc_help_switch_condition'] = 'Durumun doğasını ters çevir (VE &hArr; VEYA)';
+$lang['gpc_manage_caddie'] = 'Sepet yönetimi';
+$lang['gpc_rb_add_criterions'] = 'Kriter';
+$lang['gpc_rb_clear_criterions'] = 'Kriteri sıfırla';
+$lang['gpc_rb_do_modify_request'] = 'Aramayı değiştir';
+$lang['gpc_rb_number_of_item_found'] = 'Bulunan resim sayısı';
+$lang['gpc_rb_result_query'] = 'Arama sonucu';
+$lang['gpc_rb_search_criterion'] = 'Arama kriteri';
+$lang['gpc_rb_search'] = 'Ara';
+$lang['gpc_rb_textAND'] = 'Tüm kriterler doğrulanmalıdır';
+$lang['gpc_rb_textOR'] = 'En azından bir kriter kontrol edilmelidir';
+$lang['gpc_replace_caddie'] = 'Arama sonuçları sepettekilerin yerini aldı';
+$lang['gpc_something_is_wrong_on_the_server_side'] = 'Sunucu tarafında bir hata oluştu';
+$lang['gpc_the_caddie_is_updated'] = 'Sepet güncellendi';
+?>
