369a4a95832e4501cbf7f919aa9fbd3f117e3b83
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-04 17:38:38 +0000

    [grum_plugins_classes] Update es_ES, thanks to : jpr928
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@17746 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/plugin.lang.php
+++ language/es_ES/plugin.lang.php
@@ -15,4 +15,23 @@ $lang['Private'] = 'Privado';
 $lang['All the gallery'] = 'Toda la galeria';
 
 $lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'Algunos plugins son dependientes de otros plugins de clases superior: antes de desinstalarlos, tu tienes que desinstalar los plugins de los que dependen';
+$lang['export_Compression'] = 'Compresión';
+$lang['export_Format'] = 'Formato';
+$lang['export_Name'] = 'Nombre de fichero';
+$lang['export_dialogTitle'] = 'Exportación de datos';
+$lang['filter_and'] = 'y';
+$lang['filter_between'] = 'entre';
+$lang['filter_equal'] = 'Igual a';
+$lang['filter_ge'] = 'Mayor o igual a';
+$lang['filter_gt'] = 'Major que';
+$lang['filter_invalid'] = 'El filtro no es valido';
+$lang['filter_le'] = 'Menor o igual a';
+$lang['filter_like'] = 'Como';
+$lang['filter_lt'] = 'Menor que ';
+$lang['filter_maxValue'] = 'Valor maximal';
+$lang['filter_minValue'] = 'Valor minimal';
+$lang['filter_notEqual'] = 'Diferente que';
+$lang['filter_notLike'] = 'No como';
+$lang['filter_operator'] = 'Operador';
+$lang['filter_value'] = 'Valor';
 ?>
