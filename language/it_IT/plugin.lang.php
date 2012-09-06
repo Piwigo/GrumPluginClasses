acce4333ba24d2d7deb85fb247cb71799b47e15f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-06 21:49:41 +0000

    [grum_plugins_classes] Update it_IT, thanks to : virgigiole
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@17785 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -16,4 +16,21 @@ $lang['Public'] = 'Pubblico';
 $lang['Private'] = 'Privata';
 $lang['All the gallery'] = 'Tutta la galleria';
 
+$lang['export_Compression'] = 'Compressione';
+$lang['export_Format'] = 'Formato';
+$lang['export_Name'] = 'Nome file';
+$lang['filter_and'] = 'e';
+$lang['filter_between'] = 'Tra';
+$lang['filter_equal'] = 'Uguale a';
+$lang['filter_ge'] = 'Maggiore o uguale a';
+$lang['filter_gt'] = 'Maggiore di';
+$lang['filter_invalid'] = 'Filtro non valido';
+$lang['filter_le'] = 'Inferiore o uguale a';
+$lang['filter_lt'] = 'Inferiore a';
+$lang['filter_maxValue'] = 'Valore massimo';
+$lang['filter_minValue'] = 'Valore minimo';
+$lang['filter_notEqual'] = 'Diverso da';
+$lang['filter_notbetween'] = 'Non compreso tra';
+$lang['filter_operator'] = 'Operatore';
+$lang['filter_value'] = 'Valore';
 ?>
