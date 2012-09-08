1adc346a3cf667ef40cd6a44793872744c4c3aa3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-08 17:15:29 +0000

    [grum_plugins_classes] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@17825 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/lv_LV/plugin.lang.php
+++ language/lv_LV/plugin.lang.php
@@ -14,4 +14,24 @@ $lang['Public'] = 'Publiskie';
 $lang['Private'] = 'Privātie';
 $lang['All the gallery'] = 'Visa galerija';
 
+$lang['filter_value'] = 'Vērtība';
+$lang['filter_operator'] = 'Operators';
+$lang['filter_notLike'] = 'Nav līdzīgs';
+$lang['filter_notEqual'] = 'Atšķirīgs no';
+$lang['filter_like'] = 'Līdzīgs kā';
+$lang['export_Compression'] = 'Kompresija';
+$lang['filter_notbetween'] = 'Nav starp';
+$lang['filter_minValue'] = 'Minimālā vērtība';
+$lang['filter_maxValue'] = 'Maksimālā vērtība';
+$lang['filter_lt'] = 'Mazāks par';
+$lang['filter_invalid'] = 'Filtrs nav derīgs';
+$lang['filter_le'] = 'Mazāks vai vienāds ar';
+$lang['filter_gt'] = 'Lielāks par';
+$lang['filter_ge'] = 'Lielāks vai vienāds ar';
+$lang['filter_equal'] = 'Vienāds ar';
+$lang['filter_between'] = 'Starp';
+$lang['filter_and'] = 'un';
+$lang['export_dialogTitle'] = 'Datu eksports';
+$lang['export_Name'] = 'Faila nosaukums';
+$lang['export_Format'] = 'Formāts';
 ?>
