0bd67f806ce6c4772f6b30bc94a1506a7aad6d70
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-08 10:31:23 +0000

    [grum_plugins_classes] Update hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@17812 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/hu_HU/plugin.lang.php
+++ language/hu_HU/plugin.lang.php
@@ -13,4 +13,24 @@ $lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you 
 $lang['Public'] = 'Nyilvános';
 $lang['Private'] = 'Privát';
 $lang['All the gallery'] = 'A teljes galéria';
+$lang['filter_like'] = 'Úgy mint';
+$lang['filter_notLike'] = 'Nem úgy, mint';
+$lang['filter_notbetween'] = 'Nincs közte';
+$lang['filter_gt'] = 'Nagyobb mint';
+$lang['filter_invalid'] = 'Érvénytelen szűrő';
+$lang['filter_le'] = 'Kisebb, vagy egyenlő';
+$lang['filter_lt'] = 'Kisebb mint';
+$lang['filter_maxValue'] = 'Maximális érték';
+$lang['filter_minValue'] = 'Minimális érték';
+$lang['filter_notEqual'] = 'Eltérő';
+$lang['filter_operator'] = 'Operátor';
+$lang['filter_value'] = 'Érték';
+$lang['export_Compression'] = 'Tömörítés';
+$lang['export_Format'] = 'Formátum';
+$lang['export_Name'] = 'Fájl neve';
+$lang['export_dialogTitle'] = 'Adat export';
+$lang['filter_and'] = 'és';
+$lang['filter_between'] = 'Között';
+$lang['filter_equal'] = 'Egyenlő';
+$lang['filter_ge'] = 'Egyenlő, vagy nagyobb';
 ?>
