5afd2bc2f8c79d1021752222d84f80449f737a9c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-20 17:59:54 +0000

    [grum_plugins_classes] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@18083 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,55 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['filter_gt'] = 'Større end';
+$lang['filter_invalid'] = 'Filter er ikke gyldigt';
+$lang['filter_le'] = 'Mindre end eller lig med';
+$lang['filter_like'] = 'Det samme';
+$lang['filter_lt'] = 'Mindre end';
+$lang['filter_maxValue'] = 'Maksimal værdi';
+$lang['filter_minValue'] = 'Minimal værdi';
+$lang['filter_notEqual'] = 'Forskellig fra';
+$lang['filter_notLike'] = 'Ikke lig med';
+$lang['filter_notbetween'] = 'Ikke mellem';
+$lang['filter_operator'] = 'Operator';
+$lang['filter_value'] = 'Værdi';
+$lang['All the gallery'] = 'Hele galleriet';
+$lang['GPC required'] = 'GPC krævet';
+$lang['Infos'] = 'Info';
+$lang['Installed'] = 'Installeret';
+$lang['List of installed plugins using Grum Plugin Classes'] = 'Liste over installered plugin\'er, som anvender Grum Plugin Classes';
+$lang['List of shared modules'] = 'Liste over delte moduler';
+$lang['Module'] = 'Modul';
+$lang['Plugin'] = 'Plugin';
+$lang['Private'] = 'Privat';
+$lang['Public'] = 'Offentlig';
+$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'Nogle plugin\'er er afhængige af Grum Plugin Classes: før du afinstallerer, skal du afinstallere de afhængige plugin\'er';
+$lang['Version'] = 'Version';
+$lang['export_Compression'] = 'Komprimering';
+$lang['export_Format'] = 'Format';
+$lang['export_Name'] = 'Filnavn';
+$lang['export_dialogTitle'] = 'Dataeksport';
+$lang['filter_and'] = 'og';
+$lang['filter_between'] = 'Mellem';
+$lang['filter_equal'] = 'Lig med';
+$lang['filter_ge'] = 'Større end eller lig med';
+?>
---
+++ language/da_DK/rbuilder.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['There is no criteria ! At least, one criteria is required to do search...'] = 'Der er intet kriterium! Mindst et kriterium er krævet for at søge...';
+$lang['gpc_add_caddie'] = 'Søgeresultatet er føjet til kurven';
+$lang['gpc_help_delete_criteria'] = 'Slet kriterium';
+$lang['gpc_help_edit_criteria'] = 'Rediger kriterium';
+$lang['gpc_help_move_criteria'] = 'Træk og slip for at organisere betingelser';
+$lang['gpc_help_switch_condition'] = 'Vend betingelsen om (AND &hArr; OR)';
+$lang['gpc_manage_caddie'] = 'Håndtering af kurv';
+$lang['gpc_rb_add_criterions'] = 'Kriterium';
+$lang['gpc_rb_clear_criterions'] = 'Nulstil kriterium';
+$lang['gpc_rb_do_modify_request'] = 'Ændring af søgning';
+$lang['gpc_rb_number_of_item_found'] = 'Antal fundne billeder';
+$lang['gpc_rb_result_query'] = 'Søgeresultat';
+$lang['gpc_rb_search'] = 'Søg';
+$lang['gpc_rb_search_criterion'] = 'Søgekriterium';
+$lang['gpc_rb_textAND'] = 'Alle kriterier skal kontrolleres';
+$lang['gpc_rb_textOR'] = 'Mindst et kriterium skal kontrolleres';
+$lang['gpc_replace_caddie'] = 'Søgeresultatet erstatter kurven';
+$lang['gpc_something_is_wrong_on_the_server_side'] = 'En fejl opstod på serveren';
+$lang['gpc_the_caddie_is_updated'] = 'Kurven er opdateret';
+?>
