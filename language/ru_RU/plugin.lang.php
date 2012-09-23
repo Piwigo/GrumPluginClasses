6f74f69c40c0c1ab5a97c9cbdad0cee31b0884de
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-23 19:00:45 +0000

    [grum_plugins_classes] Update ru_RU, thanks to : rocket
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@18183 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ru_RU/plugin.lang.php
+++ language/ru_RU/plugin.lang.php
@@ -15,4 +15,24 @@ $lang['Public'] = 'Общедоступная';
 $lang['Private'] = 'Приватная';
 $lang['All the gallery'] = 'Вся галерея';
 
+$lang['export_Compression'] = 'Сжатие';
+$lang['export_Format'] = 'Формат';
+$lang['export_Name'] = 'Имя файла';
+$lang['export_dialogTitle'] = 'Экспорт данных';
+$lang['filter_and'] = 'и';
+$lang['filter_between'] = 'Между';
+$lang['filter_equal'] = 'Равно';
+$lang['filter_ge'] = 'Больше или равно';
+$lang['filter_gt'] = 'Больше';
+$lang['filter_invalid'] = 'Неверный фильтр';
+$lang['filter_le'] = 'Меньше или равно';
+$lang['filter_like'] = 'Как';
+$lang['filter_lt'] = 'Меньше';
+$lang['filter_maxValue'] = 'Максимальное значение';
+$lang['filter_minValue'] = 'Минимальное значение';
+$lang['filter_notEqual'] = 'Отличается от';
+$lang['filter_notLike'] = 'Не как';
+$lang['filter_notbetween'] = 'Не между';
+$lang['filter_operator'] = 'Оператор';
+$lang['filter_value'] = 'Значение';
 ?>
