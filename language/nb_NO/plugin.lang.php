0724a8777f1b34be4a5439320f05843c5bb3ae38
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-13 06:09:15 +0000

    [grum_plugins_classes] Update nb_NO, thanks to : abbamust
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@18631 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/nb_NO/plugin.lang.php
+++ language/nb_NO/plugin.lang.php
@@ -14,4 +14,24 @@ $lang['Public'] = 'Almenn';
 $lang['Private'] = 'Privat';
 $lang['All the gallery'] = 'Hele galleriet';
 
+$lang['export_Compression'] = 'Komprimering';
+$lang['export_Format'] = 'Format';
+$lang['export_Name'] = 'Filnavn';
+$lang['export_dialogTitle'] = 'Eksporter data';
+$lang['filter_and'] = 'og';
+$lang['filter_between'] = 'Mellom';
+$lang['filter_equal'] = 'Lik';
+$lang['filter_ge'] = 'Større eller lik';
+$lang['filter_gt'] = 'Større enn';
+$lang['filter_invalid'] = 'Ikke gyldig filter';
+$lang['filter_le'] = 'Mindre eller lik';
+$lang['filter_like'] = 'Lik';
+$lang['filter_lt'] = 'Mindre enn';
+$lang['filter_maxValue'] = 'Maksimal verdi';
+$lang['filter_minValue'] = 'Minimal verdi';
+$lang['filter_notEqual'] = 'Forskjellig fra';
+$lang['filter_notLike'] = 'Ikke lik';
+$lang['filter_notbetween'] = 'Ikke mellom';
+$lang['filter_operator'] = 'Operator';
+$lang['filter_value'] = 'Verdi';
 ?>
