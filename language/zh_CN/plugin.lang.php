a00514ffa511fc49efcba763f0bb82ee748f51bb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-28 18:24:31 +0000

    [grum_plugins_classes] Add zh_CN, thanks to : foddyy
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@18773 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,55 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['filter_gt'] = '大于';
+$lang['filter_invalid'] = '过滤器不可用';
+$lang['filter_le'] = '小于或等于';
+$lang['filter_like'] = '类似';
+$lang['filter_lt'] = '小于';
+$lang['filter_maxValue'] = '最大值';
+$lang['filter_minValue'] = '最小值';
+$lang['filter_notEqual'] = '不同于';
+$lang['filter_notLike'] = '不类似';
+$lang['filter_notbetween'] = '不介于';
+$lang['filter_operator'] = '运算';
+$lang['filter_value'] = '值';
+$lang['All the gallery'] = '所有相册';
+$lang['GPC required'] = '需要GPC';
+$lang['Infos'] = '资讯';
+$lang['Installed'] = '已安装';
+$lang['List of installed plugins using Grum Plugin Classes'] = '使用Grum Plugin Classes的已安装插件列表';
+$lang['List of shared modules'] = '共享模块列表';
+$lang['Module'] = '模块';
+$lang['Plugin'] = '插件';
+$lang['Private'] = '私有';
+$lang['Public'] = '公开';
+$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = '一些插件依赖Grum Plugin Classes：删除前，必须首先删除依赖的插件';
+$lang['Version'] = '版本';
+$lang['export_Compression'] = '压缩';
+$lang['export_Format'] = '格式';
+$lang['export_Name'] = '文件名';
+$lang['export_dialogTitle'] = '数据导出';
+$lang['filter_and'] = '和';
+$lang['filter_between'] = '介于';
+$lang['filter_equal'] = '等于';
+$lang['filter_ge'] = '大于或等于';
+?>
