92685b6cabd5223a8ed756b9975f65a52d0b422d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-03 21:43:46 +0000

    [grum_plugins_classes] Update zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@19772 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/zh_CN/rbuilder.lang.php
+++ language/zh_CN/rbuilder.lang.php
@@ -23,4 +23,20 @@
 $lang['gpc_help_edit_criteria'] = '编辑条件';
 $lang['gpc_help_delete_criteria'] = '删除条件';
 $lang['There is no criteria ! At least, one criteria is required to do search...'] = '没有条件！搜索至少需要一个条件...';
+$lang['gpc_rb_textAND'] = '符合所有条件';
+$lang['gpc_rb_textOR'] = '至少符合一个条件';
+$lang['gpc_rb_number_of_item_found'] = '找到的图片数量';
+$lang['gpc_replace_caddie'] = '以搜索结果更新标记图片库';
+$lang['gpc_the_caddie_is_updated'] = '标记图片库已更新';
+$lang['gpc_something_is_wrong_on_the_server_side'] = '服务器端发生错误';
+$lang['gpc_rb_search_criterion'] = '搜索条件';
+$lang['gpc_rb_search'] = '搜索';
+$lang['gpc_rb_result_query'] = '搜索结果';
+$lang['gpc_rb_do_modify_request'] = '修改搜索条件';
+$lang['gpc_rb_clear_criterions'] = '重置条件';
+$lang['gpc_rb_add_criterions'] = '条件';
+$lang['gpc_manage_caddie'] = '管理标记图片库';
+$lang['gpc_help_switch_condition'] = '反转条件 (AND &hArr; OR)';
+$lang['gpc_help_move_criteria'] = '鼠标拖放以安排搜索条件';
+$lang['gpc_add_caddie'] = '搜索结果已加入标记图片库';
 ?>
