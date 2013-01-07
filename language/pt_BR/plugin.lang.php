793bb17f4f3d7ca96195a952982051bf9910a84f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-07 21:39:10 +0000

    [grum_plugins_classes] Add pt_BR, thanks to : oigreslima
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@19961 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,55 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['List of shared modules'] = 'Lista de módulos compartilhados';
+$lang['Module'] = 'Modulo';
+$lang['Plugin'] = 'Plugin';
+$lang['Private'] = 'Privado';
+$lang['Public'] = 'Público';
+$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'Alguns plugins são dependentes da classe plugin Grum: antes de desinstalar, você deve primeiro desisntalar os plugins dependentes.';
+$lang['filter_gt'] = 'Maior que';
+$lang['filter_invalid'] = 'Filtro não é válido';
+$lang['filter_le'] = 'Menor ou igual a';
+$lang['filter_like'] = 'Como';
+$lang['filter_lt'] = 'Menos que';
+$lang['filter_maxValue'] = 'Valor máximo';
+$lang['filter_minValue'] = 'Valor mínimo';
+$lang['filter_notEqual'] = 'Diferente de';
+$lang['filter_notLike'] = 'Não como';
+$lang['filter_notbetween'] = 'Não entre';
+$lang['filter_operator'] = 'operador';
+$lang['filter_value'] = 'Valor';
+$lang['All the gallery'] = 'Todas as Galerias';
+$lang['GPC required'] = 'GPC necessário';
+$lang['Infos'] = 'infos';
+$lang['Installed'] = 'Instalados';
+$lang['List of installed plugins using Grum Plugin Classes'] = 'Lista de plugins instalados usando Classe Plugin Grum';
+$lang['Version'] = 'Versão';
+$lang['export_Compression'] = 'Compressão';
+$lang['export_Format'] = 'Formato';
+$lang['export_Name'] = 'Nome do arquivo';
+$lang['export_dialogTitle'] = 'Data exportação';
+$lang['filter_and'] = 'e';
+$lang['filter_between'] = 'entre';
+$lang['filter_equal'] = 'igual a';
+$lang['filter_ge'] = 'Maior ou igual a';
+?>
---
+++ language/pt_BR/rbuilder.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['There is no criteria ! At least, one criteria is required to do search...'] = 'Não existe nenhum critério! Ao menos um critério é necessário para pesquisar...';
+$lang['gpc_add_caddie'] = 'Resultado da pesquisa adicionado ao cofre';
+$lang['gpc_help_delete_criteria'] = 'Apague critério';
+$lang['gpc_help_edit_criteria'] = 'Edite critério';
+$lang['gpc_help_move_criteria'] = 'Arraste e corte para organizar as condições de pesquisa';
+$lang['gpc_help_switch_condition'] = 'Inverta natureza das condições (E &hArr; OU)';
+$lang['gpc_manage_caddie'] = 'Gerencie cofre';
+$lang['gpc_rb_add_criterions'] = 'Criério';
+$lang['gpc_rb_clear_criterions'] = 'Reinicie critério';
+$lang['gpc_rb_do_modify_request'] = 'Modifica pesquisa';
+$lang['gpc_rb_number_of_item_found'] = 'Número de imagens encontradas';
+$lang['gpc_rb_result_query'] = 'Resultado da pesquisa';
+$lang['gpc_rb_search'] = 'Pesquisar';
+$lang['gpc_rb_search_criterion'] = 'Critério de Pesquisa';
+$lang['gpc_rb_textAND'] = 'Todos os critérios devem ser satisfeitos';
+$lang['gpc_rb_textOR'] = 'Ao menos um critério deve ser satisfeito';
+$lang['gpc_replace_caddie'] = 'Resultado da pesquisa substitui o cofre';
+$lang['gpc_something_is_wrong_on_the_server_side'] = 'Um erro ocorreu no lado do servidor';
+$lang['gpc_the_caddie_is_updated'] = 'Cofre foi atualizado';
+?>
