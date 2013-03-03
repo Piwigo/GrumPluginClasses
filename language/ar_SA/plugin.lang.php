1db1f864a1ddbf3a0ef072f59dbb6841e764866e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-03 18:29:50 +0000

    [grum_plugins_classes] Update ar_SA, thanks to : dolomats
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@21141 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ar_SA/plugin.lang.php
+++ language/ar_SA/plugin.lang.php
@@ -14,4 +14,24 @@ $lang['Public'] = 'عام';
 $lang['Private'] = 'خاص';
 $lang['All the gallery'] = 'كل المعرض';
 
+$lang['export_Compression'] = 'ضغط';
+$lang['export_Format'] = 'نوع';
+$lang['export_Name'] = 'اسم الملف';
+$lang['export_dialogTitle'] = 'اسـتـنـسـاخ البيانات';
+$lang['filter_and'] = 'و';
+$lang['filter_between'] = 'ما بين';
+$lang['filter_equal'] = 'يساوي';
+$lang['filter_ge'] = 'أكبر من أو تساوي';
+$lang['filter_gt'] = 'أكبر من';
+$lang['filter_invalid'] = 'مرشح غير صالح';
+$lang['filter_le'] = 'أقل أو يساوي';
+$lang['filter_like'] = 'مثل';
+$lang['filter_lt'] = 'أقل من';
+$lang['filter_maxValue'] = 'القيمة القصوى';
+$lang['filter_minValue'] = 'القيمة الأدنى';
+$lang['filter_notEqual'] = 'مختلفة من';
+$lang['filter_notLike'] = 'ليس مثل';
+$lang['filter_notbetween'] = 'ليس بين';
+$lang['filter_operator'] = 'عامل';
+$lang['filter_value'] = 'قيمة';
 ?>
