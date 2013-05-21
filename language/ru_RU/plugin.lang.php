9aeebf40db16d352b81ad3a9c96eb0c5fd3ad642
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-05-21 10:06:43 +0000

    [grum_plugins_classes] Update ru_RU, thanks to : Konve
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@22792 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ru_RU/plugin.lang.php
+++ language/ru_RU/plugin.lang.php
@@ -4,11 +4,11 @@ $lang['Module'] = 'Модуль';
 $lang['Version'] = 'Версия';
 $lang['List of shared modules'] = 'Список общих модулей';
 $lang['List of installed plugins using Grum Plugin Classes'] = 'Список установленных модулей, использующих GPC';
-$lang['Plugin'] = 'Модуль';
+$lang['Plugin'] = 'Плагин ';
 $lang['GPC required'] = 'Требуется GPC';
 $lang['Installed'] = 'Установлен';
 $lang['Infos'] = 'Информация';
-$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'Некоторые модули зависят от Grum Plugin Classes: перед удалением вам нужно сначала удалить их';
+$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'Некоторые плагины зависят от Grum Plugin Classes: перед удалением Вам нужно сначала удалить их';
 
 // gpc > 3.3.0
 $lang['Public'] = 'Общедоступная';
