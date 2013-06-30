378a5f517dac7b46f0e2b89b9af8f06965957bdb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-30 10:38:12 +0000

    [grum_plugins_classes] Add bg_BG, thanks to : turnokopa
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@23706 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,55 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'Някои плъгини са зависими от Grum Plugin Classes: преди деинсталиране, трябва първо да деинсталирате зависимите плъгини';
+$lang['List of installed plugins using Grum Plugin Classes'] = 'Списък на инсталираните плъгини използващи Grum Plugin Classes';
+$lang['List of shared modules'] = 'Списък на споделените модули';
+$lang['filter_le'] = 'По-малко или равно на';
+$lang['filter_ge'] = 'По-голямо или равно на';
+$lang['filter_equal'] = 'Равно на';
+$lang['filter_notEqual'] = 'Различно от';
+$lang['filter_maxValue'] = 'Минимална стойност';
+$lang['filter_minValue'] = 'Максимална стойност';
+$lang['filter_lt'] = 'По-малъко от';
+$lang['filter_gt'] = 'По-голямо от';
+$lang['filter_notLike'] = 'Не харесва';
+$lang['filter_like'] = 'Харесва';
+$lang['filter_invalid'] = 'Филтъра е невалиден';
+$lang['filter_notbetween'] = 'Не е между';
+$lang['filter_value'] = 'Стойност';
+$lang['filter_operator'] = 'Опиратор';
+$lang['Infos'] = 'Информация';
+$lang['Installed'] = 'Инсталирано';
+$lang['All the gallery'] = 'Всички галерии';
+$lang['GPC required'] = 'изисква GPC';
+$lang['filter_between'] = 'Между';
+$lang['filter_and'] = 'и';
+$lang['export_Name'] = 'Име на файл';
+$lang['export_dialogTitle'] = 'Дата на експортиране';
+$lang['export_Format'] = 'Формат';
+$lang['Version'] = 'Версия';
+$lang['export_Compression'] = 'Компресия';
+$lang['Private'] = 'Частен';
+$lang['Public'] = 'Публичен';
+$lang['Module'] = 'Модул';
+$lang['Plugin'] = 'Плъгин';
+?>
---
+++ language/bg_BG/rbuilder.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['gpc_add_caddie'] = 'Търсения резултат е добавя в Caddie';
+$lang['gpc_help_switch_condition'] = 'Обръщане на естеството на състоянието(И &hArr; ИЛИ)';
+$lang['gpc_something_is_wrong_on_the_server_side'] = 'Възникна грешка от страна на сървъра';
+$lang['gpc_the_caddie_is_updated'] = 'Caddie е обновена';
+$lang['gpc_replace_caddie'] = 'Резултата от търсенето е заменен в Caddie';
+$lang['gpc_rb_textOR'] = 'Най-малко един критерии трябва да бъде избран
+';
+$lang['gpc_manage_caddie'] = 'Caddie управление';
+$lang['gpc_rb_number_of_item_found'] = 'Брой намерени снимки';
+$lang['gpc_help_move_criteria'] = 'Влачите и пускате да организира условия за търсене ';
+$lang['There is no criteria ! At least, one criteria is required to do search...'] = 'Няма критерии! Поне един критерии е необходим за да се направи търсене ...';
+$lang['gpc_help_delete_criteria'] = 'Изтриване на критериите';
+$lang['gpc_rb_textAND'] = 'Всички критерии трябва да бъдат проверени';
+$lang['gpc_rb_search_criterion'] = 'Критерии за търсене';
+$lang['gpc_rb_result_query'] = 'Резултати от търсенето';
+$lang['gpc_help_edit_criteria'] = 'Редактиране на критериите';
+$lang['gpc_rb_add_criterions'] = 'Критерии';
+$lang['gpc_rb_search'] = 'Търсене';
+$lang['gpc_rb_clear_criterions'] = 'Нулирани на критериите';
+$lang['gpc_rb_do_modify_request'] = 'Промяна на търсенето';
+?>
