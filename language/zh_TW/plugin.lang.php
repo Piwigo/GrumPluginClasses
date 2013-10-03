3e1d874ef7569161db1e563e7f0ad4734996db05
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-03 18:28:17 +0000

    [grum_plugins_classes] Add zh_TW, thanks to : kelyiu3000
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@24739 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,55 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['filter_gt'] = '大於';
+$lang['filter_invalid'] = '過濾不正確';
+$lang['filter_le'] = '小於或等於';
+$lang['filter_like'] = '類似';
+$lang['filter_lt'] = '小於';
+$lang['filter_maxValue'] = '最大值';
+$lang['filter_minValue'] = '最小值';
+$lang['filter_notEqual'] = '不同於';
+$lang['filter_notLike'] = '不類似';
+$lang['filter_notbetween'] = '不介於';
+$lang['filter_operator'] = '運算';
+$lang['filter_value'] = '值';
+$lang['filter_between'] = '介於';
+$lang['filter_equal'] = '等於';
+$lang['filter_ge'] = '大於或等於';
+$lang['filter_and'] = '和';
+$lang['export_dialogTitle'] = '數據導出';
+$lang['export_Name'] = '檔案名';
+$lang['export_Format'] = '格式';
+$lang['export_Compression'] = '壓縮';
+$lang['Version'] = '版本';
+$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = '一些插件依賴Grum Plugin Classes：刪除前，必須先刪除依賴的插件';
+$lang['Public'] = '公開';
+$lang['Private'] = '私有';
+$lang['Plugin'] = '插件';
+$lang['List of shared modules'] = '已共享模組列表';
+$lang['Module'] = '模組';
+$lang['List of installed plugins using Grum Plugin Classes'] = '使用Grum Plugin Classes中的插件列表';
+$lang['Installed'] = '已安裝';
+$lang['Infos'] = '信息';
+$lang['GPC required'] = '所需GPC版本';
+$lang['All the gallery'] = '所有相册';
+?>
---
+++ language/zh_TW/rbuilder.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['There is no criteria ! At least, one criteria is required to do search...'] = '沒有條件！搜索至少需要一個條件...';
+$lang['gpc_add_caddie'] = '搜索結果已加入標記圖片庫';
+$lang['gpc_help_delete_criteria'] = '刪除條件';
+$lang['gpc_help_edit_criteria'] = '編輯條件';
+$lang['gpc_help_move_criteria'] = '鼠標拖放以安排搜索條件';
+$lang['gpc_help_switch_condition'] = '反轉條件 (AND &hArr; OR)';
+$lang['gpc_manage_caddie'] = '管理標記圖片庫';
+$lang['gpc_rb_add_criterions'] = '條件';
+$lang['gpc_rb_clear_criterions'] = '重置條件';
+$lang['gpc_rb_do_modify_request'] = '修改搜索條件';
+$lang['gpc_rb_number_of_item_found'] = '找到的圖片數量';
+$lang['gpc_rb_result_query'] = '搜索結果';
+$lang['gpc_rb_search'] = '搜索';
+$lang['gpc_rb_search_criterion'] = '搜索條件';
+$lang['gpc_rb_textAND'] = '條件需要更正';
+$lang['gpc_rb_textOR'] = '至少符合一個條件';
+$lang['gpc_replace_caddie'] = '以搜索結果更新標記圖片庫';
+$lang['gpc_something_is_wrong_on_the_server_side'] = '伺務器端發生錯誤';
+$lang['gpc_the_caddie_is_updated'] = '標記圖片庫已更新';
+?>
