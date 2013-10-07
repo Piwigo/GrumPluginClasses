608b3c88391c2f5e82bc8ffa6da87937fb73d424
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-07 16:19:30 +0000

    [grum_plugins_classes] Update vi_VN, thanks to : dummy
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@24798 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/vi_VN/plugin.lang.php
+++ language/vi_VN/plugin.lang.php
@@ -15,4 +15,24 @@ $lang['Public'] = 'Chung';
 $lang['Private'] = 'Riêng tư';
 $lang['All the gallery'] = 'Tất cả các thư viện';
 
+$lang['export_Compression'] = 'Nén';
+$lang['export_Format'] = 'Định dạng';
+$lang['export_Name'] = 'Tên file';
+$lang['export_dialogTitle'] = 'Xuất dữ liệu';
+$lang['filter_and'] = 'và';
+$lang['filter_between'] = 'Giữa';
+$lang['filter_equal'] = 'Bằng';
+$lang['filter_ge'] = 'Lớn hơn hoặc bằng';
+$lang['filter_gt'] = 'Lớn hơn';
+$lang['filter_invalid'] = 'Bộ lọc không hợp lệ';
+$lang['filter_le'] = 'Nhỏ hơn hoặc bằng';
+$lang['filter_like'] = 'Giống';
+$lang['filter_lt'] = 'Nhỏ hơn';
+$lang['filter_maxValue'] = 'Giá trị lớn nhất';
+$lang['filter_minValue'] = 'Giá trị nhỏ nhất';
+$lang['filter_notEqual'] = 'Khác với';
+$lang['filter_notLike'] = 'Không giống';
+$lang['filter_notbetween'] = 'Không nằm giữa';
+$lang['filter_operator'] = 'Toán tử';
+$lang['filter_value'] = 'Giá trị';
 ?>
