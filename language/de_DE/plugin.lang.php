36c0711aab9d9cface9b516c2d9524c2306acb5b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-12-03 06:54:39 +0000

    [grum_plugins_classes] Update de_DE, thanks to bigant
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@30668 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/de_DE/plugin.lang.php
+++ language/de_DE/plugin.lang.php
@@ -5,20 +5,20 @@ $lang['Version'] = 'Version';
 $lang['List of shared modules'] = 'Liste der zur Verfügung gestellten Module';
 $lang['List of installed plugins using Grum Plugin Classes'] = 'Liste der installierten Erweiterungen, die Grum Plugin Classes nutzen';
 $lang['Plugin'] = 'Erweiterung';
-$lang['GPC required'] = 'Benötigte GPC Version';
+$lang['GPC required'] = 'Benötigte GPC-Version';
 $lang['Installed'] = 'Installiert am';
 $lang['Infos'] = 'Informationen';
-$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'Einige Erweiterungen benötigen Grum Plugin Classes: Sie müssen diese de-installieren, bevor Sie Grum Plugin Classes entfernen';
+$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'Einige Erweiterungen benötigen Grum Plugin Classes: Sie müssen diese deinstallieren, bevor Sie Grum Plugin Classes entfernen können';
 
 // gpc > 3.3.0
-$lang['Public'] = 'Oeffentlich';
+$lang['Public'] = 'Öffentlich';
 $lang['Private'] = 'Privat';
 $lang['All the gallery'] = 'Gesamte Galerie'; 
 
-$lang['export_Compression'] = 'Kompremierung';
+$lang['export_Compression'] = 'Komprimierung';
 $lang['export_Format'] = 'Format';
-$lang['export_Name'] = 'Datei Name';
-$lang['export_dialogTitle'] = 'Daten export';
+$lang['export_Name'] = 'Dateiname';
+$lang['export_dialogTitle'] = 'Datenexport';
 $lang['filter_and'] = 'und';
 $lang['filter_between'] = 'Zwischen';
 $lang['filter_equal'] = 'Gleich wie';
@@ -26,13 +26,12 @@ $lang['filter_ge'] = 'Größer oder Gleich als';
 $lang['filter_gt'] = 'Größer als';
 $lang['filter_invalid'] = 'Filter ist nicht gültig';
 $lang['filter_le'] = 'Weniger oder Gleich als';
-$lang['filter_like'] = 'gleich';
+$lang['filter_like'] = 'Gleich';
 $lang['filter_lt'] = 'Weniger als';
-$lang['filter_maxValue'] = 'Maximal Wert';
-$lang['filter_minValue'] = 'Minimal Wert';
+$lang['filter_maxValue'] = 'Maximalwert';
+$lang['filter_minValue'] = 'Minimalwert';
 $lang['filter_notEqual'] = 'Anders als';
 $lang['filter_notLike'] = 'Nicht gleich';
 $lang['filter_notbetween'] = 'Nicht zwischen';
 $lang['filter_operator'] = 'Anwender';
 $lang['filter_value'] = 'Wert';
-?>
