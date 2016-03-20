621472128de6d69d19c7fc53f2805515c98cd354
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-03-20 23:09:32 +0000

    Update ja_JP, thanks to ryoon
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@31479 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ja_JP/plugin.lang.php
+++ language/ja_JP/plugin.lang.php
@@ -12,4 +12,23 @@ $lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you 
 $lang['Private'] = 'プライベート';
 $lang['Public'] = 'パブリック';
 $lang['All the gallery'] = '全てのギャラリー';
-?>
+$lang['export_Compression'] = '圧縮';
+$lang['export_Format'] = '形式';
+$lang['export_Name'] = 'ファイル名';
+$lang['export_dialogTitle'] = 'データのエクスポート';
+$lang['filter_and'] = 'と';
+$lang['filter_between'] = '間';
+$lang['filter_equal'] = '等しい';
+$lang['filter_ge'] = '以上';
+$lang['filter_gt'] = 'より大きい';
+$lang['filter_invalid'] = 'フィルターが適切ではありません';
+$lang['filter_le'] = '以下';
+$lang['filter_like'] = '近い';
+$lang['filter_lt'] = 'より小さい';
+$lang['filter_maxValue'] = '最大値';
+$lang['filter_minValue'] = '最小値';
+$lang['filter_notEqual'] = '違う';
+$lang['filter_notLike'] = '近くない';
+$lang['filter_notbetween'] = '以外';
+$lang['filter_operator'] = 'オペレーター';
+$lang['filter_value'] = '値';
