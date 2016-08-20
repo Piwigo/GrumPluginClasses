cae8482e9f1735c613ff66f3fcecb8169af22d74
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-08-20 05:55:50 +0000

    Update nl_NL, thanks to watertoren5044
    git-svn-id: http://piwigo.org/svn/extensions/GrumPluginClasses@31628 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -7,21 +7,21 @@ $lang['Plugin'] = 'Plugin';
 $lang['GPC required'] = 'GPC nodig';
 $lang['Installed'] = 'Installeer-datum';
 $lang['Infos'] = 'Informatie';
-$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'Sommige plugins zijn afhankelijk van Grum Plugin Classes: voordat u deinstalleerd moet u de afhankelijke plugins deinstalleren';
+$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'Sommige plugins zijn afhankelijk van Grum Plugin Classes: voordat u deinstalleert moet u de afhankelijke plugins deinstalleren';
 
 $lang['Public'] = 'Publiek';
-$lang['Private'] = 'Prive';
-$lang['All the gallery'] = 'Heel de galerij';
+$lang['Private'] = 'Privé';
+$lang['All the gallery'] = 'Hele galerie';
 
 $lang['filter_le'] = 'Hoogstens';
-$lang['filter_lt'] = 'minder dan';
-$lang['filter_maxValue'] = 'maximale waarde';
-$lang['filter_minValue'] = 'minimale waarde';
+$lang['filter_lt'] = 'Minder dan';
+$lang['filter_maxValue'] = 'Maximale waarde';
+$lang['filter_minValue'] = 'Minimale waarde';
 $lang['filter_equal'] = 'gelijk aan';
 $lang['filter_ge'] = 'Groter of gelijk aan';
 $lang['filter_gt'] = 'Groter dan';
 $lang['filter_invalid'] = 'Filter is niet geldig';
-$lang['export_dialogTitle'] = 'Gegevens overdracht';
+$lang['export_dialogTitle'] = 'Gegevens export';
 $lang['filter_and'] = 'en';
 $lang['export_Name'] = 'Bestandnaam';
 $lang['filter_operator'] = 'Bewerkingsteken';
@@ -33,4 +33,3 @@ $lang['filter_notbetween'] = 'Niet tussen';
 $lang['filter_between'] = 'Tussen';
 $lang['filter_like'] = 'Zoals';
 $lang['filter_value'] = 'Waarde';
-?>
