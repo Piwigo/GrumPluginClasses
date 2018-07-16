4f1216769a3bb21d3562345b9bbfd9674c828c5a
plegall <plg@piwigo.org>
2018-07-16 18:28:40 +0200

    prepare version 3.5.10

--- gpc_version.inc.php
+++ gpc_version.inc.php
@@ -14,6 +14,6 @@
 
   if(!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
 
-  if(!defined('GPC_VERSION')) define('GPC_VERSION',  '3.5.9');
-  if(!defined('GPC_VERSION2')) define('GPC_VERSION2', '03.05.09');
+  if(!defined('GPC_VERSION')) define('GPC_VERSION',  '3.5.10');
+  if(!defined('GPC_VERSION2')) define('GPC_VERSION2', '03.05.10');
 ?>
--- main.inc.php
+++ main.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Grum Plugins Classes.3
-Version: 3.5.9
+Version: 3.5.10
 Description: Collection de classes partagées entre mes plugins (existants, ou à venir) / Partaged classes between my plugins (actuals or futures)
 Plugin URI: http://piwigo.org/ext/extension_view.php?eid=199
 Author: grum@piwigo.org
@@ -195,6 +195,8 @@ Author URI: http://www.grum.fr/
 | 3.5.8   | 2014/11/27 | * fix jQuery bug on attr('checked')
 |         |            |
 | 3.5.9   | 2016/03/08 | * Compatibility with Piwigo 2.8
+|         |            |
+| 3.5.10  | 2018/07/16 | * Compatibility with PHP 7.2
 |         |            |
 |         |            | ===== Don't forget to update the plugin version ! =====
 |         |            |
