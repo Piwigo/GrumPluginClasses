c6f044f558d3051c4af9bf555f773fb93becaafb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-24 14:46:58 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Version'] = 'Verzija';
+$lang['export_Compression'] = 'Kompresija';
+$lang['export_Format'] = 'Format';
+$lang['export_Name'] = 'Ime datoteke';
+$lang['export_dialogTitle'] = 'Izvoz podatkov';
+$lang['filter_invalid'] = 'Filter ni veljaven';
+$lang['filter_notLike'] = 'Ne kot';
+$lang['filter_like'] = 'Kot';
+$lang['filter_and'] = 'in';
+$lang['filter_between'] = 'Med';
+$lang['filter_notbetween'] = 'Ne med';
+$lang['filter_notEqual'] = 'Drugačen kot';
+$lang['filter_equal'] = 'Enako kot';
+$lang['filter_le'] = 'Manj ali enako';
+$lang['filter_ge'] = 'Večje ali enako';
+$lang['filter_lt'] = 'Manj kot';
+$lang['filter_gt'] = 'Večji kot';
+$lang['filter_maxValue'] = 'Največja vrednost';
+$lang['filter_minValue'] = 'Minimalna vrednost';
+$lang['filter_value'] = 'Vrednost';
+$lang['filter_operator'] = 'Upravljavec';
+$lang['All the gallery'] = 'Vsa galerija';
+$lang['GPC required'] = 'GPC je potreben';
+$lang['Infos'] = 'Informacije';
+$lang['Installed'] = 'Nameščeno';
+$lang['List of installed plugins using Grum Plugin Classes'] = 'Seznam nameščenih vtičnikov, ki uporabljajo razrede vtičnikov Grum';
+$lang['List of shared modules'] = 'Seznam skupnih modulov';
+$lang['Module'] = 'Modul';
+$lang['Plugin'] = 'Vtičnik';
+$lang['Private'] = 'Zasebno';
+$lang['Public'] = 'Javno';
+$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'Nekateri vtičniki so odvisni od razredov vtičnikov Grum: pred odstranitvijo morate najprej odstraniti odvisne vtičnike';
---
+++ language/sl_SI/rbuilder.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['gpc_help_move_criteria'] = 'Povlecite in spustite, da organizirate pogoje iskanja';
+$lang['gpc_help_switch_condition'] = 'Obrni naravo pogoja (IN &hArr; ALI)';
+$lang['gpc_help_delete_criteria'] = 'Izbriši kriterije';
+$lang['gpc_help_edit_criteria'] = 'Uredite kriterije';
+$lang['There is no criteria ! At least, one criteria is required to do search...'] = 'Ni kriterijev! Za iskanje je potreben vsaj en kriterij...';
+$lang['gpc_something_is_wrong_on_the_server_side'] = 'Na strani strežnika je prišlo do napake';
+$lang['gpc_the_caddie_is_updated'] = 'Nosač je bil posodobljen';
+$lang['gpc_add_caddie'] = 'Rezultat iskanja je dodan v nosača';
+$lang['gpc_replace_caddie'] = 'Rezultat iskanja zamenja nosač';
+$lang['gpc_manage_caddie'] = 'Upravljanje nosača';
+$lang['gpc_rb_textOR'] = 'Preveriti je treba vsaj en kriterij';
+$lang['gpc_rb_textAND'] = 'Vsa kriterija morajo biti preverjena';
+$lang['gpc_rb_search'] = 'Iskanje';
+$lang['gpc_rb_clear_criterions'] = 'Ponastavi kriterije';
+$lang['gpc_rb_add_criterions'] = 'Kriterij';
+$lang['gpc_rb_number_of_item_found'] = 'Število najdenih slik';
+$lang['gpc_rb_result_query'] = 'Rezultat iskanja';
+$lang['gpc_rb_do_modify_request'] = 'Spremenite iskanje';
+$lang['gpc_rb_search_criterion'] = 'Kriteriji iskanja';
