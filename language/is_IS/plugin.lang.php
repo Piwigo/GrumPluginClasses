5f6092c0bbc74435b54957e5ecb6847f20932473
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-29 11:47:23 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['All the gallery'] = 'Allt galleríið';
+$lang['GPC required'] = 'GPC krafist';
+$lang['Infos'] = 'Upplýsingar';
+$lang['Installed'] = 'Uppsett';
