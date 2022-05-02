4585c8fc9a6b67b546425e812b9baaa3857431ff
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-05-02 06:19:20 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/rbuilder.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['There is no criteria ! At least, one criteria is required to do search...'] = 'Það eru engin viðmið! Að minnsta kosti eitt skilyrði þarf til að framkvæma leit...';
+$lang['gpc_add_caddie'] = 'Leitarniðurstöðu er bætt við í kerrunni';
+$lang['gpc_help_delete_criteria'] = 'Eyða forsendum';
+$lang['gpc_help_edit_criteria'] = 'Breyta forsendum';
+$lang['gpc_help_move_criteria'] = 'Dragðu og slepptu til að skipuleggja leitarskilyrði';
+$lang['gpc_help_switch_condition'] = 'Snúið við eðli ástandsins (AND &hArr; OR)';
+$lang['gpc_manage_caddie'] = 'Körfustjórnun';
+$lang['gpc_rb_add_criterions'] = 'Viðmið';
+$lang['gpc_rb_clear_criterions'] = 'Endirsetja viðmið';
+$lang['gpc_rb_do_modify_request'] = 'Breyta leit';
+$lang['gpc_rb_number_of_item_found'] = 'Fjöldi mynda fundinn';
+$lang['gpc_rb_result_query'] = 'Leitarniðurstaða';
+$lang['gpc_rb_search'] = 'Leita';
+$lang['gpc_rb_search_criterion'] = 'Leitarskilyrði';
+$lang['gpc_rb_textAND'] = 'Öll viðmið verða að vera staðfest';
+$lang['gpc_rb_textOR'] = 'Athuga þarf að minnsta kosti eina viðmiðun';
+$lang['gpc_replace_caddie'] = 'Leitarniðurstaða kemur í stað körfunar';
+$lang['gpc_something_is_wrong_on_the_server_side'] = 'Villa hefur komið upp á þjóninum';
+$lang['gpc_the_caddie_is_updated'] = 'Karfa var uppfærð';
