6effcaf4edd48e608a499a4a3c2e3c06e388f40a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 06:31:26 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['filter_gt'] = 'גדול מ';
+$lang['filter_invalid'] = 'המסנן אינו תקף';
+$lang['filter_le'] = 'קטן או שווה ל';
+$lang['filter_like'] = 'כמו';
+$lang['filter_lt'] = 'פחות מ';
+$lang['filter_maxValue'] = 'ערך מקסימלי';
+$lang['filter_minValue'] = 'ערך מינימלי';
+$lang['filter_notEqual'] = 'שונה מ';
+$lang['filter_notLike'] = 'לא כמו';
+$lang['filter_notbetween'] = 'לא בין';
+$lang['filter_operator'] = 'אופרטור';
+$lang['filter_value'] = 'ערך';
+$lang['All the gallery'] = 'כל הגלריה';
+$lang['GPC required'] = 'נדרש GPC';
+$lang['Infos'] = 'מידע';
+$lang['Installed'] = 'מותקן';
+$lang['List of installed plugins using Grum Plugin Classes'] = 'רשימת התוספים המותקנים שמשתמשים ב-Grum Plugin Classes';
+$lang['List of shared modules'] = 'רשימת המודולים המשותפים';
+$lang['Module'] = 'מודול';
+$lang['Plugin'] = 'תוסף';
+$lang['Private'] = 'פרטי';
+$lang['Public'] = 'ציבורי';
+$lang['Some plugins are dependent on Grum Plugin Classes: before uninstall, you must first uninstall the plugins dependent'] = 'חלק מהתוספים תלויים ב-Grum Plugin Classes: לפני ההסרה, עליך להסיר תחילה את התוספים התלויים';
+$lang['Version'] = 'גרסה';
+$lang['export_Compression'] = 'דחיסה';
+$lang['export_Format'] = 'פורמט';
+$lang['export_Name'] = 'שם קובץ';
+$lang['export_dialogTitle'] = 'ייצוא נתונים';
+$lang['filter_and'] = 'ו';
+$lang['filter_between'] = 'בין';
+$lang['filter_equal'] = 'שווה ל';
+$lang['filter_ge'] = 'גדול או שווה ל';
---
+++ language/he_IL/rbuilder.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['There is no criteria ! At least, one criteria is required to do search...'] = '!אין קריטריון. לפחות קריטריון אחד נדרש כדי לבצע חיפוש...';
+$lang['gpc_add_caddie'] = 'תוצאת החיפוש נוספה לסל הקניות';
+$lang['gpc_help_delete_criteria'] = 'מחק קריטריון';
+$lang['gpc_help_edit_criteria'] = 'ערוך קריטריון';
+$lang['gpc_help_move_criteria'] = 'גרור ושחרר כדי לארגן תנאי חיפוש';
+$lang['gpc_help_switch_condition'] = 'הפוך את טבע התנאי (AND &hArr; OR)';
+$lang['gpc_manage_caddie'] = 'ניהול סל הקניות';
+$lang['gpc_rb_add_criterions'] = 'קריטריונים';
+$lang['gpc_rb_clear_criterions'] = 'אפס קריטריונים';
+$lang['gpc_rb_do_modify_request'] = 'שנה חיפוש';
+$lang['gpc_rb_number_of_item_found'] = 'מספר התמונות שנמצאו';
+$lang['gpc_rb_result_query'] = 'תוצאת החיפוש';
+$lang['gpc_rb_search'] = 'חיפוש';
+$lang['gpc_rb_search_criterion'] = 'קריטריוני חיפוש';
+$lang['gpc_rb_textAND'] = 'יש לוודא את כל הקריטריונים';
+$lang['gpc_rb_textOR'] = 'לפחות קריטריון אחד חייב להיות מסומן';
+$lang['gpc_replace_caddie'] = 'תוצאת החיפוש מחליף את סל הקניות';
+$lang['gpc_something_is_wrong_on_the_server_side'] = 'שגיאה התרחשה בצד השרת';
+$lang['gpc_the_caddie_is_updated'] = 'סל הקניות עודכן';
